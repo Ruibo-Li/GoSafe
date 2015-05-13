a3ac40552df547b94c32ee161191ae86fcac83b2
pennlio <pennlio@gmail.com>
2015-05-13 04:46:25 -0400

    fix type

--- Front/gosafe/js/index_functions.js
+++ Front/gosafe/js/index_functions.js
@@ -5,16 +5,16 @@ function myReport() {
     // Json object
     info.crime_time = document.getElementById("crime_time").value
     info.crime_date = document.getElementById("crime_date").value
-    info.type = document.getElementById("crime_type").value
+    info.crime_type = document.getElementById("crime_type").value
     info.address = document.getElementById("crime_loc").value
     info.zipcode = document.getElementById("crime_zip").value
-    if (info.type == "select crime type") info.type = ""
+    if (info.crime_type == "select crime type") info.crime_type = ""
     // info.zipcode = document.getElementById("crime_zipcode").value
 
 
     document.getElementById("rpt_time").innerHTML = "Time: " + info.crime_time;
     document.getElementById("rpt_date").innerHTML = "Date: " + info.crime_date;
-    document.getElementById("rpt_type").innerHTML = "Crime Type: " + info.type;
+    document.getElementById("rpt_type").innerHTML = "Crime Type: " + info.crime_type;
     document.getElementById("rpt_loc").innerHTML = "Location: " + info.address;
     document.getElementById("rpt_zip").innerHTML = "Zipcode: " + info.zipcode;
 
