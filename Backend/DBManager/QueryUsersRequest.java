43cb6b51a2349063a0e914636f9e44676ea8cb24
Yi Guo <yi.guo@yahoo.com>
2015-05-25 10:40:58 -0600

    Update QueryUsersRequest.java

--- Backend/DBManager/QueryUsersRequest.java
+++ Backend/DBManager/QueryUsersRequest.java
@@ -23,7 +23,7 @@ public class QueryUsersRequest {
 	private AmazonDynamoDBClient dynamo;
 	
 	public QueryUsersRequest() throws JSONException {
-		AWSCredentials credentials = new BasicAWSCredentials("AKIAJOEIWOBW6JQAF3IA", "2uxZjNVpKGOKT/KBZn32Y7jFXjT+l76X72Gnwa8R");
+		AWSCredentials credentials = new BasicAWSCredentials("", "");
 		dynamo = new AmazonDynamoDBClient(credentials);
 		dynamo.setEndpoint("dynamodb.us-west-2.amazonaws.com");
 		table = new DynamoDB(dynamo).getTable("users");
